a821b6d8f80f6ecd9ffe4e825e96228c97d61fcc
patt812 <dev.patt812@gmail.com>
2024-09-22 02:15:34 +0900

    AC AOJ ITP1 5-B

---
+++ AOJ/ITP1/5/5b.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+#define rep(i, n) for (int i = 0; i < n; ++i)
+using ll = long long;
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  int x, y;
+  while (1) {
+    cin >> x >> y;
+    if (x + y == 0) break;
+    rep(i, x) {
+      rep(j, y) {
+        if (i != 0 && i != x - 1 && j != 0 && j != y - 1) {
+          cout << '.';
+        } else {
+          cout << '#';
+        }
+      }
+      cout << "\n";
+    }
+    cout << "\n";
+  }
+}
