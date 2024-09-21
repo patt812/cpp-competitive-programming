63f5623e3edb1bd9fb9488935fc0ba6b1b009d79
patt812 <dev.patt812@gmail.com>
2024-09-22 02:12:30 +0900

    AC AOJ ITP1 3-A

---
+++ AOJ/ITP1/3/3a.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define rep(i, n) for (int i = 0; i < n; ++i)
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+
+  rep(_, 1000) cout << "Hello World\n";
+}
