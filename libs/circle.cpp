45c04fe9986b7886695758d07885a00dfaa7f82b
patt812 <dev.patt812@gmail.com>
2024-09-23 16:24:27 +0900

    Add circle library toRAD

---
+++ libs/circle.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#define _GLIBCXX_DEBUG
+#define rep(i, n) for (int i = 0; i < n; ++i)
+using ll = long long;
+using namespace std;
+
+// Convert degree to radian
+double toRAD(double degree) { return degree * numbers::pi / 180.0; }
