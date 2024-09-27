dc22c71b16e68ece917366819151677c12139cf6
patt812 <dev.patt812@gmail.com>
2024-09-28 01:52:17 +0900

    Add array library insertionSort

--- libs/array.cpp
+++ libs/array.cpp
@@ -44,3 +44,36 @@ int findNthLargestIndex(vector<int>& nums, int n) {
 
   return quickSelect(nums, indices, 0, size - 1, n - 1);
 }
+
+// Sort array by selection sort
+void insertionSort(vector<int>& a) {
+  int n = a.size();
+  for (int i = 1; i < n; i++) {
+    int v = a[i];
+    int j = i - 1;
+    while (j >= 0 && a[j] > v) {
+      a[j + 1] = a[j];
+      j--;
+    }
+    a[j + 1] = v;
+  }
+}
+
+// Sort array by selection sort in descending order
+void insertionSort(vector<int>& a, bool descending) {
+  if (!descending) {
+    insertionSort(a);
+    return;
+  }
+
+  int n = a.size();
+  for (int i = 1; i < n; i++) {
+    int v = a[i];
+    int j = i - 1;
+    while (j >= 0 && a[j] < v) {
+      a[j + 1] = a[j];
+      j--;
+    }
+    a[j + 1] = v;
+  }
+}
